ae3644d92d5fbd7270ea7553139434bc88253416
bonuelc <bonuelc@gmail.com>
2016-11-07 10:55:49 -0700

    Initial Commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Christopher Bonuel on 11/7/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
