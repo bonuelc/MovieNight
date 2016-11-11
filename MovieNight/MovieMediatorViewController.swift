0a3f158c8c81bd3ad472dfa5155a440fc4f2c599
bonuelc <bonuelc@gmail.com>
2016-11-11 12:08:43 -0700

    Add a blank MovieMediatorViewController

---
+++ MovieNight/MovieMediatorViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieMediatorViewController.swift
+//  MovieNight
+//
+//  Created by Christopher Bonuel on 11/9/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class MovieMediatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
