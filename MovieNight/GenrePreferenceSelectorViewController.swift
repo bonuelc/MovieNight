7074c327d5dba7630ed6f0b51d48972a44535897
bonuelc <bonuelc@gmail.com>
2016-11-15 11:56:44 -0700

    Rename ViewController -> GenrePreferenceSelectorViewController
    # Conflicts:
    #	MovieNight.xcodeproj/project.pbxproj

--- MovieNight/GenrePreferenceSelectorViewController.swift
+++ MovieNight/GenrePreferenceSelectorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GenrePreferenceSelectorViewController
 //  MovieNight
 //
 //  Created by Christopher Bonuel on 11/7/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GenrePreferenceSelectorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
