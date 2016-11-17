038a8c16088b4fd73b02aa61d8588cca776047c0
bonuelc <bonuelc@gmail.com>
2016-11-17 14:28:49 -0700

    Add a ResultsTableViewController

---
+++ MovieNight/ResultsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsTableViewController.swift
+//  MovieNight
+//
+//  Created by Christopher Bonuel on 11/17/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewController: UITableViewController {
+    
+    var data: [String]!
+    
+    // MARK: - Table view data source
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("resultCell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+     
+         return cell
+     }
+}
